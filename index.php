aa8ad881d3f76d93348ce5773f043ee8ef58acb0
arindam7013 <47492331+arindam7013@users.noreply.github.com>
2020-06-13 00:55:25 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
+<pre>
 <?php
 
 print `ifconfig`
+  <img class="lazy" data-src="https://content.thriveglobal.com/wp-content/uploads/2019/10/shutterstock_678583375.jpg" alt="Cute-Puppy" style="width:500px;height:333px;">
 
 ?>
+</pre>
